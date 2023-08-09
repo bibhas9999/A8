11ab344d3b3d3106696266c54faf0ccdb72845fa
bibhas9999 <141627658+bibhas9999@users.noreply.github.com>
2023-08-09 19:42:54 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+//Node JS in Hindi #8 All About Package.json file
+//package json store module and store prohect all details (coding)
+
+
+//A9 chap
+//if i delet node-module what i do?
+//sol-- go through terminal and commant npm install
+
+
+//always not push node_module folder thast why creat a new folder .gitignore
